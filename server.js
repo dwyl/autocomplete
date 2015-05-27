98830e67481b1381e4f65b32613c5813cb0efa8f
nelsonic <contact.nelsonic@gmail.com>
2015-05-27 10:58:58 +0100

    implements /find/word test and api endpoint

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var http = require('http');
 var port = process.env.PORT || 3000;
 var ac = require('./index.js');
+ac.import(function(err, count) {
+  console.log("imported a bunch of words! >> ", count.length);
+});
+
 var fs = require('fs');
 var index = fs.readFileSync(__dirname + '/index.html');
 
@@ -11,7 +15,19 @@ http.createServer(function handler(request, response) {
     response.writeHead(200, {"Content-Type": "text/html"});
     response.end(index.toString());
   }
-  response.end('hello world!');
+  if (url.indexOf('/find/') > -1) {
+    // locahost:3000/find/word
+    var word = url.split('/')[2];
+    // console.log(word);
+    ac.findWord(word, function (err, found){
+      // console.log(found);
+      response.end(found.join(','));
+    });
+    // response.end('word: ', word);
+  }
+  else {
+    response.end('hello Dan!');
+  }
 
 }).listen(port);
 
